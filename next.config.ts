00148a4f4716ccfadd11fc208666f07b86c2d80a
Malli <malli@MacBookPro.ip>
2026-05-21 17:36:58 +1200

    build: map environment variableex

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
+  env: {
+    ADDRESSABLE_API_KEY: process.env.ADDRESSABLE_API_KEY,
+  },
   /* config options here */
-};
+} as any;
 
 export default nextConfig;
