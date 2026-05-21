2d86f3f0990a802e2f94fdce0a8a2c91de30f4be
Malli <malli@MacBookPro.ip>
2026-05-21 18:06:12 +1200

    build: clean next.config and transition env management to amplify.yml pipeline

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: any = {
-  env: {
-    ADDRESSABLE_API_KEY: process.env.ADDRESSABLE_API_KEY,
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
